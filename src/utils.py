10fe0a4572f519dd40eb0883d1cf46e5361a5d27
danie <daniel.tischler@mni.thm.de>
2024-02-21 19:31:05 +0100

    ADDED Function for Folder Hierachy representation TO_ADD_LATER Add function to add comments for explaining folder contents

--- src/utils.py
+++ src/utils.py
@@ -35,3 +35,46 @@ def load_data(path: str, decimal: str) -> pd.DataFrame:
                      )
 
     return df
+
+def generate_markdown_tree(root_path: str, file_extensions: list=None, indent=0) -> str:
+    """
+    Generate a markdown tree of the given directory
+    Only used for documentation purposes
+
+    :param root_path: Path to the root directory
+    :param file_extensions: List of file extensions to include in the tree
+    :param indent: Indentation level for the tree
+    :return: Markdown representation of the directory tree
+    """
+    tree = ''
+    files = []
+    directories = []
+
+    for item in os.listdir(root_path):
+        item_path = os.path.join(root_path, item)
+
+        if os.path.isdir(item_path):
+            if not item.startswith("__") and not item.startswith(".") and not item.startswith("runs"):
+                directories.append(item)
+        else:
+            files.append(item)
+
+    for directory in sorted(directories):
+        tree += f"{'|   ' * indent}├── {directory}\\\n"
+        subtree = generate_markdown_tree(os.path.join(root_path, directory), file_extensions, indent + 1)
+        tree += subtree
+
+    for file in sorted(files):
+        if file_extensions is None or any(file.endswith(ext) for ext in file_extensions):
+            tree += f"{'|   ' * indent}├── {file}\\\n"
+
+    return tree
+
+
+if __name__ == '__main__':
+
+    # Example usage
+    project_root = ".."
+    markdown_tree = generate_markdown_tree(project_root)
+
+    print(markdown_tree)
