fd7a6f9665312d30b362ccdca1898f8588a33a81
danie <daniel.tischler@mni.thm.de>
2024-02-21 22:20:56 +0100

    ADDED Documentations and refactored code

--- src/logging_helper.py
+++ src/logging_helper.py
@@ -13,6 +13,11 @@ logger = logging.getLogger(__name__)
 
 
 def setup_logging(config_path: str):
+    """
+    Sets the logging configuration for the project
+
+    :param config_path: path to the logging configuration file
+    """
     config_file = pathlib.Path(config_path)
     with open(config_file) as file:
         config = json.load(file)
@@ -20,37 +25,65 @@ def setup_logging(config_path: str):
 
 
 def get_handler(handler_name: str) -> logging.handlers:
-    for handler in logger.handlers:
+    """
+    Get the handler by name
+
+    :param handler_name: handler name to get
+    """
+    for handler in logging.getLogger().handlers:
         if handler.name == handler_name:
             return handler
 
 
-def print_regression_metrics(y_true, y_pred):
-    logger.info(f"[bold green]Regression metrics: \n"
+def change_file_handler_path(handler_name: str, new_path: str):
+    """
+    Change the file path of the file handler
+
+    :param handler_name: handler name to change
+    :param new_path: new file path
+    """
+    handler = get_handler(handler_name=handler_name)
+    # Close the existing handler
+    handler.close()
+
+    # Create a new FileHandler with the desired file path
+    new_handler = logging.FileHandler(new_path)
+    new_handler.setLevel(handler.level)
+    new_handler.setFormatter(handler.formatter)
+
+    # Replace the old handler with the new one
+    logger = logging.getLogger()
+    logger.removeHandler(handler)
+    logger.addHandler(new_handler)
+
+
+def print_regression_metrics(y_true, y_pred) -> str:
+    return(f"Regression metrics: \n"
                 f"    -> R2:  {r2_score(y_true=y_true, y_pred=y_pred)}\n"
                 f"    -> MAE: {mean_absolute_error(y_true=y_true, y_pred=y_pred)}\n"
                 f"    -> MSE: {mean_squared_error(y_true=y_true, y_pred=y_pred)}\n"
                 f"    -> VAR: {explained_variance_score(y_true=y_true, y_pred=y_pred)}\n")
 
 
-def generate_run_directories(tag):
+def generate_run_directories(log_name: str, tag: str = ""):
     """
     Create directories for the run
     Also sets the log file path for the logger
 
     """
     main_dir = init_dir(root_dir="../runs", tag=tag)
-    plot_dir = os.path.join(main_dir, "plots")
+    plot_dir = os.path.join(main_dir, "plots/")
     model_dir = os.path.join(main_dir, "models/")
+    log_dir = os.path.join(main_dir, "logs/")
     os.mkdir(plot_dir)
     os.mkdir(model_dir)
-    handler = get_handler(handler_name="stdout")
-    logger.baseFilename = os.path.join(main_dir, "log.log")
+    os.mkdir(log_dir)
+    change_file_handler_path(handler_name="file", new_path=log_dir + f"{log_name}.log")
 
-    return main_dir, model_dir, plot_dir
+    return main_dir, model_dir, plot_dir, log_dir
 
 
-def init_dir(root_dir: str = "../runs", tag: str = "") -> str:
+def init_dir(tag: str, root_dir: str = "../runs") -> str:
     """
     Create a directory for the run
 
