9d1d70c92d3d314b0a5f7f00b4b141b63473a7ce
danie <daniel.tischler@mni.thm.de>
2024-02-22 20:57:39 +0100

    REFORMAT

--- src/config.py
+++ src/config.py
@@ -2,8 +2,4 @@ from sklearn.ensemble import RandomForestRegressor
 
 rf_regressor = RandomForestRegressor()
 
-rf_regressor_param_grid = {
-    'n_estimators': [100],
-    'min_samples_split': [3],
-    'min_samples_leaf': [1],
-}
+rf_regressor_param_grid = {'n_estimators': [1000], 'min_samples_split': [5], 'min_samples_leaf': [5], }
